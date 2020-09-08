a7036171033be610df4b3e35efffee21e9fb26d1
Daysy Lorena <daysy.lorena.ismart@gmail.com>
2020-09-08 11:58:58 -0300

    atividade CRUD 08.09

---
+++ dez/src/main/java/com/farmacia/dez/DezApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.dez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DezApplication.class, args);
+	}
+
+}
